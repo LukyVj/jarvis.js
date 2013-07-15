50a16250c8848a7c8fd1b1cb6a24a57d9dbc1e0c
WebGate <lucas.bonomi@gmail.com>
2013-07-16 01:01:04 +0200

    Jarvis.js V1.1
    Modify CSS animation.
    Bottom loader added.
    Subtile Start-screen added

--- 1.0/jarvisjs-1.0/jarvis.js
+++ 1.0/jarvisjs-1.0/jarvis.js
@@ -1,8 +1,8 @@
 /*
 == Jarvis.JS  ========
 component of Jarvis.js
-Version : 1.1; 
-Date : 07/16/13
+Version : 1.0; 
+Date : 07/15/13
 Author : @LukyVj
 
 Under M.I.T License : 
@@ -11,22 +11,17 @@ dev.web-gate.fr/MIT
 */
 $(document).ready(function(){
 $('body').attr('id','on-jarvis').addClass('stocked');
-        $("head").append("<link>");     
-        $("body").append("<div id='loader'></div>");
-        $("body").append("<div id='st-screen'></div>");
-        $("#loader").addClass('load');
+        $("head").append("<link>");
         var css = $("head").children(":last");
         css.attr({
           rel:  "stylesheet",
           type: "text/css",
-          href: "jarvisjs-1.1/jarvis.css"
+          href: "jarvisjs-1.0/jarvis.css"
         });
-         setTimeout(function(){
-	          $("#st-screen").fadeOut();
-        },300)
         setTimeout(function(){
 	           $("#on-jarvis").removeClass('stocked');
         },500)
+
   var numJarvisParts = $('.jarvis').length;
 $("#on-jarvis").each(function() {
          $('.jarvis').addClass(function (index) {
